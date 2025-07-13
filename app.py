fb2362be176e45ca4eacb289fda844680d3b58db
Abishek Mishra <168964561+AbishekMishra183@users.noreply.github.com>
2025-07-13 19:57:54 +0545

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,27 @@
-# input feilds haru banaune ho 
+import streamlit as st
+from predict import predict_diabetes
 
-import streamlit as st 
 
 st.set_page_config(
-    page_title='Diabetes Predection App'
-
+    page_title='Diabetes Prediction APP',
+    icon = 'A'
 )
 
-st.title('Diabiates Predection')
-st.header('Input you data:')
+st.title('Diabetes Prediction')
+st.header('Input your data:')
 
-Gulcose=st.number_input('Gulcose',min_value=0,max_value=400,value=20)
-blood_pressure=st.number_input('Blood Pressure',0,200,0)
+glucose = st.number_input('Glucose',min_value=0, max_value=400, value=20)
+blod_pressure = st.number_input('Blood Pressure', 0, 200, 0)
+dfp = st.number_input('DFP', 0, 20, 0)
+age = st.number_input('Age', 20, 80, 40)
+
+if st.button('Test Diabetes'):
+    input_data = [glucose, blod_pressure, dfp, age]
+    result = predict_diabetes(input_data)
+    print(result)
+
+    if result[0] == 1:
+        st.warning('High probability of Diabetes')
+        st.warning('See your doctor')
+    else:
+        st.success('Low chance of diabetes**')
