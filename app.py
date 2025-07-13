7874b3c9d6f1e97b89f32747ab2b1d1896d66726
Abishek Mishra <168964561+AbishekMishra183@users.noreply.github.com>
2025-07-13 19:59:37 +0545

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,25 @@
 import streamlit as st
-from predict import predict_diabetes
+from predict import predict_diabetes #file ko name predict_diabets fumction lai cl gareko
+#if we import filename only then we call by using filename.function_name
+st.set_page_config(page_title='Diabetes Preidicition App')
+
+st.title('Diabetes Predicition')
+st.header('Input your Data:')
 
 
-st.set_page_config(
-    page_title='Diabetes Prediction APP',
-    icon = 'A'
-)
 
-st.title('Diabetes Prediction')
-st.header('Input your data:')
+glucose=st.number_input('Glucose', min_value=0 , max_value=400,value=20)
+blood_pressure=st.number_input('Blood_Pressure' , 0, 200, 0)
+dfp=st.number_input('DFP' , 0, 20, 0)
+age=st.number_input('Age',20,80,40)
+st.sidebar.header("hihiihihihihihihihihihiiih")
+if st.button('Test Diabetes'): # button le k k kaam garney yah mention huna parcha
+  input_data=[glucose, blood_pressure,dfp,age]
+  result=predict_diabetes(input_data)
+  print(result)
 
-glucose = st.number_input('Glucose',min_value=0, max_value=400, value=20)
-blod_pressure = st.number_input('Blood Pressure', 0, 200, 0)
-dfp = st.number_input('DFP', 0, 20, 0)
-age = st.number_input('Age', 20, 80, 40)
+  if result[0]==1 :
+    st.error('High probability of Diabetes')
 
-if st.button('Test Diabetes'):
-    input_data = [glucose, blod_pressure, dfp, age]
-    result = predict_diabetes(input_data)
-    print(result)
-
-    if result[0] == 1:
-        st.warning('High probability of Diabetes')
-        st.warning('See your doctor')
-    else:
-        st.success('Low chance of diabetes**')
+  else :
+    st.error("Congratulation you have no diabetes")
